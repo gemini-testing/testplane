84b6c0ea1925bb5e36eeb46bfc6e154b3736ced3
Anton Usmansky <cody0@yandex-team.ru>
2016-02-15 18:18:33 +0300

    Plugin support

--- test/lib/e2e-runner.js
+++ test/lib/e2e-runner.js
@@ -3,6 +3,8 @@
 var E2ERunner = require('../../lib/e2e-runner'),
     MainRunner = require('../../lib/runner'),
     RunnerEvents = require('../../lib/constants/runner-events'),
+    plugins = require('../../lib/plugins'),
+    RunnerFacade = require('../../lib/e2e-runner-facade'),
     utils = require('../utils'),
     EventEmitter = require('events').EventEmitter;
 
@@ -13,6 +15,8 @@ describe('e2e-runner', function() {
         beforeEach(function() {
             sandbox.stub(MainRunner, 'create');
             MainRunner.create.returns(sinon.createStubInstance(MainRunner));
+
+            sandbox.stub(plugins);
         });
 
         afterEach(function() {
@@ -22,14 +26,37 @@ describe('e2e-runner', function() {
         function stubMainRunner_(runFn) {
             var mainRunner = new EventEmitter();
 
-            mainRunner.run = sandbox.stub(MainRunner.prototype, 'run', runFn.bind(null, mainRunner));
+            mainRunner.run = sandbox.stub(MainRunner.prototype, 'run', runFn && runFn.bind(null, mainRunner));
             MainRunner.create.returns(mainRunner);
+            return mainRunner;
         }
 
         function run_() {
             return new E2ERunner(utils.makeConfigStub())
                 .run();
         }
+
+        describe('load plugins', function() {
+            it('should load plugins', function() {
+                return run_()
+                    .then(function() {
+                        assert.calledWith(plugins.load, sinon.match.instanceOf(RunnerFacade));
+                    });
+            });
+
+            it('should create facade with runner and config', function() {
+                var config = utils.makeConfigStub(),
+                    e2eRunner = new E2ERunner(config),
+                    mainRunner = stubMainRunner_();
+
+                sandbox.stub(RunnerFacade.prototype, '__constructor');
+
+                return e2eRunner.run()
+                    .then(function() {
+                        assert.calledWith(RunnerFacade.prototype.__constructor, mainRunner, config);
+                    });
+            });
+        });
 
         it('should return true if there are no failed tests', function() {
             return run_()
