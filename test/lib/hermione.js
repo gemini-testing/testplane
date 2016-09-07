2404cb05bfaef90c747dbe96b935ba9cccea97e8
DudaGod <dudkevich@yandex-team.ru>
2016-09-07 09:55:00 +0300

    fix failing tests in hermione.js and rewrite to es6

--- test/lib/hermione.js
+++ test/lib/hermione.js
@@ -1,95 +1,85 @@
 'use strict';
 
-var Hermione = require('../../lib/hermione'),
-    Runner = require('../../lib/runner'),
-    RunnerEvents = require('../../lib/constants/runner-events'),
-    plugins = require('../../lib/plugins'),
-    RunnerFacade = require('../../lib/hermione-facade'),
-    utils = require('../utils'),
-    EventEmitter = require('events').EventEmitter;
+const EventEmitter = require('events').EventEmitter;
 
-const globExtra = require('glob-extra');
 const q = require('q');
+const globExtra = require('glob-extra');
 
-describe('hermione', function() {
-    var sandbox = sinon.sandbox.create();
+const Hermione = require('../../lib/hermione');
+const plugins = require('../../lib/plugins');
+const Runner = require('../../lib/runner');
+const RunnerEvents = require('../../lib/constants/runner-events');
+const RunnerFacade = require('../../lib/hermione-facade');
+const utils = require('../utils');
 
-    describe('run', function() {
-        beforeEach(function() {
+describe('hermione', () => {
+    const sandbox = sinon.sandbox.create();
+
+    describe('run', () => {
+        beforeEach(() => {
             sandbox.stub(Runner, 'create');
             sandbox.stub(globExtra, 'expandPaths').returns(q([]));
 
             Runner.create.returns(sinon.createStubInstance(Runner));
 
+            sandbox.stub(globExtra, 'expandPaths').returns(q([]));
+
             sandbox.stub(plugins);
         });
 
-        afterEach(function() {
-            sandbox.restore();
-        });
+        afterEach(() => sandbox.restore());
 
-        function stubRunner_(runFn) {
-            var runner = new EventEmitter();
+        const stubRunner_ = (runFn) => {
+            const runner = new EventEmitter();
 
             runner.run = sandbox.stub(Runner.prototype, 'run', runFn && runFn.bind(null, runner));
             Runner.create.returns(runner);
             return runner;
-        }
+        };
 
-        function run_() {
-            return new Hermione(utils.makeConfigStub())
-                .run();
-        }
+        const run_ = () => new Hermione(utils.makeConfigStub()).run();
 
-        describe('load plugins', function() {
-            it('should load plugins', function() {
+        describe('load plugins', () => {
+            it('should load plugins', () => {
                 return run_()
-                    .then(function() {
+                    .then(() => {
                         assert.calledWith(plugins.load, sinon.match.instanceOf(RunnerFacade));
                     });
             });
 
-            it('should create facade with runner and config', function() {
-                var config = utils.makeConfigStub(),
-                    hermione = new Hermione(config),
-                    runner = stubRunner_();
+            it('should create facade with runner and config', () => {
+                const config = utils.makeConfigStub();
+                const hermione = new Hermione(config);
+                const runner = stubRunner_();
 
                 sandbox.stub(RunnerFacade.prototype, '__constructor');
 
                 return hermione.run()
-                    .then(function() {
+                    .then(() => {
                         assert.calledWith(RunnerFacade.prototype.__constructor, runner, config);
                     });
             });
         });
 
-        it('should return true if there are no failed tests', function() {
+        it('should return true if there are no failed tests', () => {
             return run_()
-                .then(function(success) {
-                    assert.ok(success);
-                });
+                .then((success) => assert.ok(success));
         });
 
-        it('should return false if there are failed tests', function() {
-            stubRunner_(function(runner) {
+        it('should return false if there are failed tests', () => {
+            stubRunner_((runner) => {
                 runner.emit(RunnerEvents.TEST_FAIL);
             });
 
             return run_()
-                .then(function(success) {
-                    assert.isFalse(success);
-                });
+                .then((success) => assert.isFalse(success));
         });
 
-        it('should return false if there were some errors', function() {
-            stubRunner_(function(runner) {
-                runner.emit(RunnerEvents.ERROR);
-            });
+        it('should return false if there were some errors', () => {
+            stubRunner_((runner) => runner.emit(RunnerEvents.ERROR));
 
             return run_()
-                .then(function(success) {
-                    assert.isFalse(success);
-                });
+                .then((success) => assert.isFalse(success));
         });
     });
 });
