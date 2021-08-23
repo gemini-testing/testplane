fosemberg/hermione

feat: hermione constructor can take configuration as an object

lib/config/index.js
@@ -8,8 +8,8 @@ const defaults = require('./defaults');
 const BrowserConfig = require('./browser-config');
 
 module.exports = class Config {
-    static create(configPath) {
-        return new Config(configPath);
+    static create(configOrConfigPath) {
+        return new Config(configOrConfigPath);
     }
 
     static read(configPath) {
@@ -31,11 +31,17 @@ module.exports = class Config {
         }
     }
 
-    constructor(configPath = defaults.config) {
-        this.configPath = configPath;
+    constructor(configOrConfigPath = defaults.config) {
+        let options;
+        if (_.isObjectLike(configOrConfigPath)) {
+            options = configOrConfigPath;
+        } else if (typeof configOrConfigPath === 'string') {
+            this.configPath = configOrConfigPath;
+            options = Config.read(configOrConfigPath);
+        }
 
         _.extend(this, parseOptions({
-            options: Config.read(configPath),
+            options,
             env: process.env,
             argv: process.argv
         }));

lib/hermione.js
@@ -15,8 +15,8 @@ const validateUnknownBrowsers = require('./validators').validateUnknownBrowsers;
 const logger = require('./utils/logger');
 
 module.exports = class Hermione extends BaseHermione {
-    constructor(configPath) {
-        super(configPath);
+    constructor(configOrConfigPath) {
+        super(configOrConfigPath);
 
         this._failed = false;
     }

test/lib/config/index.js
@@ -13,15 +13,18 @@ describe('config', () => {
     const initConfig = (opts) => {
         opts = opts || {};
         parseOptions = sandbox.stub().returns(opts.configParserReturns);
+        const configOrConfigPath = opts.configOrConfigPath || defaults.config;
+        const stubs = {
+            './options': parseOptions
+        };
 
-        const configPath = opts.configPath || defaults.config;
-        const resolvedConfigPath = path.resolve(process.cwd(), configPath);
-        const Config = proxyquire('../../../lib/config', {
-            './options': parseOptions,
-            [resolvedConfigPath]: opts.requireConfigReturns || {}
-        });
+        if (typeof configOrConfigPath === 'string') {
+            const resolvedConfigPath = path.resolve(process.cwd(), configOrConfigPath);
+            stubs[resolvedConfigPath] = opts.requireConfigReturns || {};
+        }
+        const Config = proxyquire('../../../lib/config', stubs);
 
-        return Config.create(opts.configPath, opts.allowOverrides);
+        return Config.create(opts.configOrConfigPath, opts.allowOverrides);
     };
 
     afterEach(() => sandbox.restore());
@@ -39,12 +42,18 @@ describe('config', () => {
             assert.calledWithMatch(parseOptions, {options: 'some-options', env: process.env, argv: process.argv});
         });
 
+        it('should parse config from object', () => {
+            initConfig({configOrConfigPath: {someOption: 'some-value'}});
+
+            assert.calledWithMatch(parseOptions, {options: {someOption: 'some-value'}, env: process.env, argv: process.argv});
+        });
+
         it('should create config', () => {
             assert.include(initConfig({configParserReturns: {some: 'option'}}), {some: 'option'});
         });
 
         it('should extend config with a config path', () => {
-            assert.include(initConfig({configPath: 'config-path'}), {configPath: 'config-path'});
+            assert.include(initConfig({configOrConfigPath: 'config-path'}), {configPath: 'config-path'});
         });
 
         it('should wrap browser config with "BrowserConfig" instance', () => {

gemini-testing/testplane

lib/config/index.js
@@ -8,21 +8,11 @@ const defaults = require('./defaults');
 const BrowserConfig = require('./browser-config');
 
 module.exports = class Config {
-    static create(configPath) {
-        return new Config(configPath);
+    static create(config) {
+        return new Config(config);
     }
 
     static read(configPath) {
-        const config = Config._getConfigFromFile(configPath);
-
-        if (_.isFunction(config.prepareEnvironment)) {
-            config.prepareEnvironment();
-        }
-
-        return config;
-    }
-
-    static _getConfigFromFile(configPath) {
         try {
             return require(path.resolve(process.cwd(), configPath));
         } catch (e) {
@@ -31,11 +21,21 @@ module.exports = class Config {
         }
     }
 
-    constructor(configPath = defaults.config) {
-        this.configPath = configPath;
+    constructor(config = defaults.config) {
+        let options;
+        if (_.isObjectLike(config)) {
+            options = config;
+        } else {
+            this.configPath = config;
+            options = Config.read(config);
+        }
+
+        if (_.isFunction(options.prepareEnvironment)) {
+            options.prepareEnvironment();
+        }
 
         _.extend(this, parseOptions({
-            options: Config.read(configPath),
+            options,
             env: process.env,
             argv: process.argv
         }));

lib/hermione.js
@@ -15,8 +15,8 @@ const validateUnknownBrowsers = require('./validators').validateUnknownBrowsers;
 const logger = require('./utils/logger');
 
 module.exports = class Hermione extends BaseHermione {
-    constructor(configPath) {
-        super(configPath);
+    constructor(config) {
+        super(config);
 
         this._failed = false;
     }

test/lib/config/index.js
@@ -13,15 +13,18 @@ describe('config', () => {
     const initConfig = (opts) => {
         opts = opts || {};
         parseOptions = sandbox.stub().returns(opts.configParserReturns);
+        const config = opts.config || defaults.config;
+        const stubs = {
+            './options': parseOptions
+        };
 
-        const configPath = opts.configPath || defaults.config;
-        const resolvedConfigPath = path.resolve(process.cwd(), configPath);
-        const Config = proxyquire('../../../lib/config', {
-            './options': parseOptions,
-            [resolvedConfigPath]: opts.requireConfigReturns || {}
-        });
+        if (typeof config === 'string') {
+            const resolvedConfigPath = path.resolve(process.cwd(), config);
+            stubs[resolvedConfigPath] = opts.requireConfigReturns || {};
+        }
+        const Config = proxyquire('../../../lib/config', stubs);
 
-        return Config.create(opts.configPath, opts.allowOverrides);
+        return Config.create(opts.config, opts.allowOverrides);
     };
 
     afterEach(() => sandbox.restore());
@@ -39,12 +42,18 @@ describe('config', () => {
             assert.calledWithMatch(parseOptions, {options: 'some-options', env: process.env, argv: process.argv});
         });
 
+        it('should parse config from object', () => {
+            initConfig({config: {someOption: 'some-value'}});
+
+            assert.calledWithMatch(parseOptions, {options: {someOption: 'some-value'}, env: process.env, argv: process.argv});
+        });
+
         it('should create config', () => {
             assert.include(initConfig({configParserReturns: {some: 'option'}}), {some: 'option'});
         });
 
         it('should extend config with a config path', () => {
-            assert.include(initConfig({configPath: 'config-path'}), {configPath: 'config-path'});
+            assert.include(initConfig({config: 'config-path'}), {configPath: 'config-path'});
         });
 
         it('should wrap browser config with "BrowserConfig" instance', () => {