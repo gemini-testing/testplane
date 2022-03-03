83aef96d0b8aacad7cd79746508d53275b2123e7
Nikolai Markov <shadowusr@yandex-team.ru>
2022-03-03 14:36:24 +0300

    chore: add typings for beforeEach and afterEach hooks

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,2 +1,4 @@
 declare const hermione: Hermione.GlobalHelper;
 declare const it: Hermione.TestDefinition;
+declare const beforeEach: Hermione.TestHookDefinition;
+declare const afterEach: Hermione.TestHookDefinition;
