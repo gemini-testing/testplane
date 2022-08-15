1e18fedae9cfdbb066ec69b66baad798ac37e4ba
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2022-08-15 18:09:32 +0300

    test(browser/commands/get-config): remove unnecessary unit tests

--- test/lib/browser/commands/get-config/index.js
+++ test/lib/browser/commands/get-config/index.js
@@ -14,43 +14,12 @@ describe('"getConfig" command', () => {
 
     afterEach(() => sandbox.restore());
 
-    it('should exist', async () => {
-        await mkBrowser_().init();
-
-        assert.isFunction(session.getConfig);
-    });
-
     it('should return object', async () => {
         await mkBrowser_().init();
 
+        assert.isFunction(session.getConfig);
         const browserConfig = session.getConfig();
         assert.isObject(browserConfig);
-    });
-
-    it('should have defined desiredCapabilities', async () => {
-        await mkBrowser_({
-            desiredCapabilities: {
-                killProcessByName: true,
-                honorSystemProxy: false,
-                ensureCleanSession: true
-            }
-        }).init();
-
-        const browserConfig = session.getConfig();
-        assert.isDefined(browserConfig.desiredCapabilities);
-        assert.isObject(browserConfig.desiredCapabilities);
-        assert.nestedPropertyVal(browserConfig, 'desiredCapabilities.killProcessByName', true);
-        assert.nestedPropertyVal(browserConfig, 'desiredCapabilities.honorSystemProxy', false);
-        assert.nestedPropertyVal(browserConfig, 'desiredCapabilities.ensureCleanSession', true);
-    });
-
-    it('should have defined gridUrl', async () => {
-        await mkBrowser_({
-            gridUrl: 'http://test_new_host:1234/wd/hub?query=value'
-        }).init();
-
-        const browserConfig = session.getConfig();
-        assert.equal(browserConfig.gridUrl, 'http://test_new_host:1234/wd/hub?query=value');
     });
 
     it('should have defined baseUrl', async () => {
