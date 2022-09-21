a10b2063bf9a75d7fc7c734f8a39160def14f269
oldskytree <oldskytree@yandex-team.ru>
2022-09-21 23:26:35 +0300

    chore(core): rewrite temp on typescript

--- lib/core/temp/index.ts
+++ lib/core/temp/index.ts
@@ -1,13 +1,20 @@
-'use strict';
-
-const temp = require('temp');
-const path = require('path');
-const _ = require('lodash');
+import temp, { AffixOptions } from 'temp';
+import path from 'path';
+import _ from 'lodash';
 
 temp.track();
 
+type TempConstructorOpts = {
+    attach?: boolean;
+};
+type SerializedTemp = {
+    dir: string;
+};
+
 class Temp {
-    constructor(dir, opts = {}) {
+    private _tempDir: string;
+
+    constructor(dir: string, opts: TempConstructorOpts = {}) {
         this._tempDir = opts.attach
             ? dir
             : temp.mkdirSync({
@@ -16,31 +23,32 @@ class Temp {
             });
     }
 
-    path(opts = {}) {
+    path(opts: AffixOptions = {}): string {
         return temp.path(_.extend(opts, {
             dir: this._tempDir
         }));
     }
 
-    serialize() {
+    serialize(): SerializedTemp {
         return {dir: this._tempDir};
     }
 }
 
-let tempInstance;
-module.exports = {
-    init: (dir) => {
+let tempInstance: Temp;
+
+export default {
+    init: (dir: string): void => {
         if (!tempInstance) {
             tempInstance = new Temp(dir);
         }
     },
 
-    attach: (serializedTemp) => {
+    attach: (serializedTemp: SerializedTemp): void => {
         if (!tempInstance) {
             tempInstance = new Temp(serializedTemp.dir, {attach: true});
         }
     },
 
-    path: (opts) => tempInstance.path(opts),
-    serialize: () => tempInstance.serialize()
+    path: (opts: AffixOptions): string => tempInstance.path(opts),
+    serialize: (): SerializedTemp => tempInstance.serialize()
 };
