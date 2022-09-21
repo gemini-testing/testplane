cb63c03f78e5c8907ec2e9350bfa7a15376e16bd
oldskytree <oldskytree@yandex-team.ru>
2022-09-21 23:34:32 +0300

    chore(core): rewrite config to typescript

--- lib/core/config/options.ts
+++ lib/core/config/options.ts
@@ -1,16 +1,21 @@
-'use strict';
+import _ from 'lodash';
+import { section, map, option } from 'gemini-configparser';
 
-const _ = require('lodash');
-const configparser = require('gemini-configparser');
+export type SetConfig = {
+    files: Array<string>;
+    ignoreFiles: Array<string>;
+    browsers: Array<string>;
+};
 
-const section = configparser.section;
-const option = configparser.option;
-const map = configparser.map;
+export type SetsConfig = {
+    [platform: string]: SetConfig;
+};
 
-module.exports = {
-    sets: map(section({
+export default {
+    sets: map<SetsConfig, any>(section({
         files: option({
-            validate: (value) => {
+            defaultValue: [] as Array<string>,
+            validate: (value): asserts value is Array<string> => {
                 if (!_.isArray(value) && !_.isString(value)) {
                     throw new Error('"sets.files" must be an array or string');
                 }
@@ -19,19 +24,19 @@ module.exports = {
                     throw new Error('"sets.files" must be an array of strings');
                 }
             },
-            map: (val) => [].concat(val)
+            map: (val: string | Array<string>): Array<string> => ([] as Array<string>).concat(val)
         }),
         ignoreFiles: option({
-            defaultValue: [],
-            validate: (value) => {
+            defaultValue: [] as Array<string>,
+            validate: (value): asserts value is Array<string> => {
                 if (!_.isArray(value) || !_.every(value, _.isString)) {
                     throw new Error('"sets.ignoreFiles" must be an array of strings');
                 }
             }
         }),
         browsers: option({
-            defaultValue: (config) => _.keys(config.browsers),
-            validate: (value, config) => {
+            defaultValue: (config): Array<string> => _.keys(config.browsers),
+            validate: (value, config): asserts value is Array<string> => {
                 if (!_.isArray(value)) {
                     throw new Error('"sets.browsers" must be an array');
                 }
@@ -43,6 +48,6 @@ module.exports = {
             }
         })
     }), {
-        '': {files: []} // Use `all` set with default values if sets were not specified in a config
+        '': {files: [], ignoreFiles: []} // Use `all` set with default values if sets were not specified in a config
     })
 };
