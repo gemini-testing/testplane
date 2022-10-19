1aa04415649676809a7e1943860b33d01171fbea
Anton Usmansky <j0tunn@ya.ru>
2022-10-19 15:35:07 +0300

    chore: up eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
     extends: 'gemini-testing',
-    root: true
+    root: true,
+    parserOptions: {
+        ecmaVersion: 2022
+    }
 };
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-    extends: 'gemini-testing/tests'
+    extends: 'gemini-testing/tests',
+    parserOptions: {
+        ecmaVersion: 2022
+    }
 };
