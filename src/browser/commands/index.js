2fb4474bd169c85f555569b79d0a33fd674d7141
DudaGod <dudkevich@yandex-team.ru>
2023-03-22 13:58:40 +0300

    fix: temporary overwrite "scrollIntoView" command with bug

--- src/browser/commands/index.js
+++ src/browser/commands/index.js
@@ -1,3 +1,3 @@
 "use strict";
 
-module.exports = ["assert-view", "getConfig", "getPuppeteer", "setOrientation"];
+module.exports = ["assert-view", "getConfig", "getPuppeteer", "setOrientation", "scrollIntoView"];
