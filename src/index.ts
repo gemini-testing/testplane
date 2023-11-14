22bead3c772643f4bdb8b75a4bf06c80572d8684
shadowusr <nikhakerlab@gmail.com>
2023-11-15 00:49:55 +0300

    feat: export TestCollection and Test types

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@ import "expect-webdriverio";
 import { GlobalHelper } from "./types";
 export { Hermione as default } from "./hermione";
 
-export type { WdioBrowser, TestResult, Suite } from "./types";
+export type { WdioBrowser, TestResult, Test, Suite } from "./types";
 export type { Config } from "./config";
 export type { ConfigInput } from "./config/types";
+export type { TestCollection } from "./test-collection";
 
 declare global {
     const hermione: GlobalHelper;
