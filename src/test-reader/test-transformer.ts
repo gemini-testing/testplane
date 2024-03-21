4e52ef3a0ee541f167aa30507b3f058b4e3c85ab
DudaGod <dudkevich@yandex-team.ru>
2024-03-21 16:14:48 +0300

    chore: add bundle watcher

--- src/test-reader/test-transformer.ts
+++ src/test-reader/test-transformer.ts
@@ -1,3 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+
+// TODO: use import instead require. Currently require is used because ts build filed from bundle folder even if it excluded in tsconfig
 export const setupTransformHook: () => VoidFunction = require("../bundle").setupTransformHook;
 export const TRANSFORM_EXTENSIONS: string[] = require("../bundle").TRANSFORM_EXTENSIONS;
