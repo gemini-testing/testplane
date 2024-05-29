0b073d5741e6779d8e93c3e31765a57732f419e1
DudaGod <dudkevich@yandex-team.ru>
2024-05-29 17:16:29 +0300

    fix: types for describe, it

--- typings/api.d.ts
+++ typings/api.d.ts
@@ -1,6 +1,6 @@
-type TestDefinition = import("../src/test-reader/test-object/types").TestDefinition;
-type SuiteDefinition = import("../src/test-reader/test-object/types").SuiteDefinition;
-type TestHookDefinition = import("../src/test-reader/test-object/types").TestHookDefinition;
+type TestDefinition = import("../build/src/test-reader/test-object/types").TestDefinition;
+type SuiteDefinition = import("../build/src/test-reader/test-object/types").SuiteDefinition;
+type TestHookDefinition = import("../build/src/test-reader/test-object/types").TestHookDefinition;
 
 declare const it: TestDefinition;
 declare const describe: SuiteDefinition;
