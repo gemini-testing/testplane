3b161e7ee559e4eb1bd3adfbea1595d13e7aef5e
Ivan Kabir <tu144x@gmail.com>
2024-08-12 15:49:03 +0300

    fix: fix global types

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -4,20 +4,13 @@
 /// <reference path="./clear-require.d.ts" />
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./escape-string-regexp.d.ts" />
-/// <reference types="mocha" />
+/// <reference types="expect-webdriverio" />
 /// <reference types="webdriverio" />
-
-declare namespace globalThis {
-    // eslint-disable-next-line no-var
-    var expect: ExpectWebdriverIO.Expect;
-}
 
 //Temporary workaround to get rid of Cannot find module 'rollup/parseAst' or its corresponding type declarations. There are types at '/node_modules/rollup/dist/parseAst.d.ts', but this result could not be resolved under your current 'moduleResolution' setting. Consider updating to 'node16', 'nodenext', or 'bundler'.
 declare module "rollup/parseAst" {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    export function parseAst(...args: any[]): any;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    export function parseAstAsync(...args: any[]): Promise<any>;
+    export function parseAst(...args: unknown[]): unknown;
+    export function parseAstAsync(...args: unknown[]): Promise<unknown>;
 }
 
 // remove after updating expect-webdriverio@4 (should migrate to esm first)
