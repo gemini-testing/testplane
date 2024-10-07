6023993a70fbfec55bea9375eb7c01a1ef23ff1b
DudaGod <dudkevich@yandex-team.ru>
2024-10-08 00:55:44 +0300

    fix(component-testing): correctly import components with css from third party libraries

--- src/utils/module.ts
+++ src/utils/module.ts
@@ -6,3 +6,7 @@ export const requireModule = async <T = unknown>(modulePath: string): Promise<T>
 
     return require(isModuleLocal ? path.resolve(modulePath) : modulePath);
 };
+
+export const requireModuleSync = (modulePath: string): unknown => {
+    return require(modulePath);
+};
