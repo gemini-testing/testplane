80fa4f290fe18d27a13d50c7637841c422f418d3
shadowusr <nikhakerlab@gmail.com>
2025-05-16 11:50:52 +0300

    chore: fix formatting

--- src/browser/standalone.ts
+++ src/browser/standalone.ts
@@ -10,35 +10,35 @@ import { WebdriverPool } from "../browser-pool/webdriver-pool";
 import RuntimeConfig from "../config/runtime-config";
 import type { CommonConfig } from "../config/types";
 
-export type StandaloneBrowserOptions =
-  Pick<CommonConfig, 
-    | 'automationProtocol'
-    | 'desiredCapabilities'
-    | 'gridUrl'
-    | 'baseUrl'
-    | 'httpTimeout'
-    | 'pageLoadTimeout'
-    | 'waitTimeout'
-    | 'waitInterval'
-    | 'headless'
-    | 'prepareBrowser'
-    | 'windowSize'
-    | 'orientation'
-    | 'headers'
-    | 'strictSSL'
-    | 'user'
-    | 'key'
-    | 'system'
-  >;
+export type StandaloneBrowserOptions = Pick<
+    CommonConfig,
+    | "automationProtocol"
+    | "desiredCapabilities"
+    | "gridUrl"
+    | "baseUrl"
+    | "httpTimeout"
+    | "pageLoadTimeout"
+    | "waitTimeout"
+    | "waitInterval"
+    | "headless"
+    | "prepareBrowser"
+    | "windowSize"
+    | "orientation"
+    | "headers"
+    | "strictSSL"
+    | "user"
+    | "key"
+    | "system"
+>;
 
 export type StandaloneBrowserOptionsInput = Partial<StandaloneBrowserOptions>;
 
 export async function launchBrowser(options: StandaloneBrowserOptionsInput = {}): Promise<WebdriverIO.Browser> {
     const desiredCapabilities = options.desiredCapabilities || {};
-    
+
     const browserName = desiredCapabilities.browserName || BrowserName.CHROME;
     const normalizedBrowserName = getNormalizedBrowserName(browserName) as W3CBrowserName;
-    
+
     if (!normalizedBrowserName) {
         throw new Error(
             [
@@ -47,16 +47,16 @@ export async function launchBrowser(options: StandaloneBrowserOptionsInput = {})
             ].join("\n"),
         );
     }
-    
+
     RuntimeConfig.getInstance().extend({
         debug: options.system?.debug ?? true,
         local: true,
     });
-    
+
     const browserConfig = {
         desiredCapabilities: {
             browserName,
-            ...desiredCapabilities
+            ...desiredCapabilities,
         },
         gridUrl: options.gridUrl || LOCAL_GRID_URL,
         baseUrl: options.baseUrl,
@@ -81,19 +81,19 @@ export async function launchBrowser(options: StandaloneBrowserOptionsInput = {})
         },
         system: options.system || {
             debug: true,
-        }
+        },
     });
-    
+
     const webdriverPool = new WebdriverPool();
     const emitter = new AsyncEmitter();
-    
+
     const newBrowser = new NewBrowser(config, {
         id: browserName,
         version: desiredCapabilities.browserVersion,
         emitter,
         wdPool: webdriverPool,
     });
-    
+
     await newBrowser.init();
 
     const existingBrowser = new ExistingBrowser(config, {
@@ -102,24 +102,27 @@ export async function launchBrowser(options: StandaloneBrowserOptionsInput = {})
         emitter,
         state: {},
     });
-    
-    const calibrator = new Calibrator();
-    
-    const session = newBrowser.publicAPI;
-    
-    await existingBrowser.init({
-        sessionId: session.sessionId,
-        sessionCaps: session.capabilities,
-        sessionOpts: {
-            capabilities: session.capabilities,
-            ...session.options
-        }
-    }, calibrator);
 
-    existingBrowser.publicAPI.overwriteCommand('deleteSession', async function() {
+    const calibrator = new Calibrator();
+
+    const session = newBrowser.publicAPI;
+
+    await existingBrowser.init(
+        {
+            sessionId: session.sessionId,
+            sessionCaps: session.capabilities,
+            sessionOpts: {
+                capabilities: session.capabilities,
+                ...session.options,
+            },
+        },
+        calibrator,
+    );
+
+    existingBrowser.publicAPI.overwriteCommand("deleteSession", async function () {
         await existingBrowser.quit();
         await newBrowser.kill();
     });
-    
+
     return existingBrowser.publicAPI;
 }
