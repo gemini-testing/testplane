cafa8e81132e422adf706819958544fa377c3584
shadowusr <nikhakerlab@gmail.com>
2025-07-19 23:13:51 +0300

    chore: re-write coords-validator to typescript

--- test/src/browser/screen-shooter/viewport/index.js
+++ test/src/browser/screen-shooter/viewport/index.js
@@ -4,7 +4,7 @@ const _ = require("lodash");
 
 const { Image } = require("src/image");
 const Viewport = require("src/browser/screen-shooter/viewport");
-const CoordValidator = require("src/browser/screen-shooter/viewport/coord-validator");
+const { CoordValidator } = require("src/browser/screen-shooter/viewport/coord-validator");
 
 describe("Viewport", () => {
     const sandbox = sinon.createSandbox();
