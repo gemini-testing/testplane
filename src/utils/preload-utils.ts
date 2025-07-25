c2aefa92cdeb8eac59e64145f2260f0669ccc607
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2025-07-26 02:31:29 +0300

    perf: preload test reader/parser modules in worker

--- src/utils/preload-utils.ts
+++ src/utils/preload-utils.ts
@@ -3,3 +3,11 @@ import { loadEsm } from "load-esm";
 export const preloadWebdriverIO = async (): Promise<void> => {
     await loadEsm("@testplane/webdriverio").catch(() => {});
 };
+
+export const preloadMochaReader = async (): Promise<void> => {
+    await loadEsm(require.resolve("../test-reader/mocha-reader")).catch(() => {});
+};
+
+export const preloadTestTransformer = async (): Promise<void> => {
+    await loadEsm(require.resolve("../bundle/test-transformer")).catch(() => {});
+};
