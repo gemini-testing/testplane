baed939438b0da9017aa38f02c58c8ec1170db77
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2025-07-26 14:20:40 +0300

    perf: perform isolation while preparing session

--- src/browser/existing-browser.ts
+++ src/browser/existing-browser.ts
@@ -80,6 +80,8 @@ export class ExistingBrowser extends Browser {
             this._startCollectingCustomCommands();
         }
 
+        const isolationPromise = this._performIsolation({ sessionCaps, sessionOpts });
+
         this._extendStacktrace();
         this._addSteps();
         this._addHistory();
@@ -93,7 +95,10 @@ export class ExistingBrowser extends Browser {
             "testplane: init browser",
             async () => {
                 this._addCommands();
-                await this._performIsolation({ sessionCaps, sessionOpts });
+
+                await isolationPromise;
+
+                this._callstackHistory?.clear();
 
                 try {
                     this.config.prepareBrowser && this.config.prepareBrowser(this.publicAPI);
