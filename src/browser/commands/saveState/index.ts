6084b4f45727b1633cdba769ad4514cf0f33ac19
rocketraccoon <rocketraccoon@yandex-team.ru>
2025-09-12 19:01:51 +0700

    feat(testplane): save browser state
    feat(testplane): get cookies from all page requests
    
    fix(testplane): add local storage
    
    fix(testplane): done with save data and start restore data
    
    fix(testplane): start restore data
    
    fix(testplane): start restore data
    
    fix(testplane): restore all data except indexDB
    
    fix(testplane): webdriver save state done
    
    fix(testplane): webdriver restore state
    
    fix(testplane): fix ff and add logs
    
    fix(testplane): fix tests
    
    fix(testplane): fix types
    
    fix(testplane): add save state in memory
    
    fix(testplane): add tests
    
    fix(testplane): hide save and restore indexDB
    
    fix(testplane): fix code review
    
    fix(testplane): fix code review #2
    
    fix(testplane): fix code review #3
    
    fix(testplane): fix code review #4
    
    fix(testplane): fixes
    
    fix(testplane): fixes
    
    fix(testplane): fixes

---
+++ src/browser/commands/saveState/index.ts
@@ -0,0 +1,176 @@
+import fs from "fs-extra";
+
+import type { Browser } from "../../types";
+import { dumpStorage, StorageData } from "./dumpStorage";
+import { DEVTOOLS_PROTOCOL, WEBDRIVER_PROTOCOL } from "../../../constants/config";
+import { Cookie } from "@testplane/wdio-protocols";
+import { isSupportIsolation } from "../../../utils/browser";
+import { ExistingBrowser, getActivePuppeteerPage } from "../../existing-browser";
+
+export type SaveStateOptions = {
+    path?: string;
+
+    cookies?: boolean;
+    localStorage?: boolean;
+    sessionStorage?: boolean;
+};
+
+export type FrameData = StorageData;
+
+export type SaveStateData = {
+    cookies?: Array<Cookie>;
+    framesData: Record<string, FrameData>;
+};
+
+export const defaultOptions = {
+    cookies: true,
+    localStorage: true,
+    sessionStorage: true,
+};
+
+export const getCalculatedProtocol = (browser: Browser): typeof DEVTOOLS_PROTOCOL | typeof WEBDRIVER_PROTOCOL => {
+    const protocol = browser.config.automationProtocol;
+
+    if (protocol === DEVTOOLS_PROTOCOL) {
+        return DEVTOOLS_PROTOCOL;
+    }
+
+    if (
+        protocol === WEBDRIVER_PROTOCOL &&
+        browser.config.isolation &&
+        isSupportIsolation(browser.publicAPI.capabilities.browserName!, browser.publicAPI.capabilities.browserVersion!)
+    ) {
+        return DEVTOOLS_PROTOCOL;
+    }
+
+    return protocol;
+};
+
+export const getWebdriverFrames = async (session: WebdriverIO.Browser): Promise<string[]> =>
+    session.execute<string[], []>(() =>
+        Array.from(document.getElementsByTagName("iframe"))
+            .map(el => el.getAttribute("src") as string)
+            .filter(src => src !== null && src !== "about:blank"),
+    );
+
+export default (browser: ExistingBrowser): void => {
+    const { publicAPI: session } = browser;
+
+    session.addCommand("saveState", async (_options: SaveStateOptions = {}): Promise<SaveStateData> => {
+        const options = { ...defaultOptions, ..._options };
+
+        const data: SaveStateData = {
+            framesData: {},
+        };
+
+        switch (getCalculatedProtocol(browser)) {
+            case WEBDRIVER_PROTOCOL: {
+                if (options.cookies) {
+                    const storageCookies = await session.storageGetCookies({});
+
+                    data.cookies = storageCookies.cookies.map(cookie => ({
+                        name: cookie.name,
+                        value: cookie.value.value,
+                        domain: cookie.domain,
+                        path: cookie.path,
+                        expires: cookie.expiry,
+                        httpOnly: cookie.httpOnly,
+                        secure: cookie.secure,
+                    }));
+                }
+
+                await session.switchToParentFrame();
+
+                const frames = await getWebdriverFrames(session);
+                const framesData: Record<string, FrameData> = {};
+
+                for (let i = 0; i < frames.length; i++) {
+                    await session.switchToParentFrame();
+
+                    // after last element have to get data from parent frame
+                    if (i < frames.length) {
+                        await session.switchFrame(frames[i]);
+                    }
+
+                    const origin = await session.execute<string, []>(() => window.location.origin);
+
+                    if (!origin || origin === "null" || framesData[origin]) {
+                        continue;
+                    }
+
+                    const frameData: FrameData = {};
+
+                    if (options.localStorage || options.sessionStorage) {
+                        const { localStorage, sessionStorage } = await session.execute<StorageData, []>(dumpStorage);
+
+                        if (localStorage && options.localStorage) {
+                            frameData.localStorage = localStorage;
+                        }
+
+                        if (sessionStorage && options.sessionStorage) {
+                            frameData.sessionStorage = sessionStorage;
+                        }
+                    }
+
+                    if (frameData.localStorage || frameData.sessionStorage) {
+                        framesData[origin] = frameData;
+                    }
+                }
+
+                await session.switchToParentFrame();
+
+                data.framesData = framesData;
+                break;
+            }
+            case DEVTOOLS_PROTOCOL: {
+                data.cookies = await browser.getAllRequestsCookies();
+
+                const puppeteer = await session.getPuppeteer();
+                const page = await getActivePuppeteerPage(puppeteer);
+
+                if (!page) {
+                    break;
+                }
+
+                const frames = page.frames();
+
+                const framesData: Record<string, FrameData> = {};
+
+                for (const frame of frames) {
+                    const origin = new URL(frame.url()).origin;
+
+                    if (origin === "null" || framesData[origin]) {
+                        continue;
+                    }
+
+                    const frameData: FrameData = {};
+
+                    if (options.localStorage || options.sessionStorage) {
+                        const { localStorage, sessionStorage }: StorageData = await frame.evaluate(dumpStorage);
+
+                        if (localStorage && options.localStorage) {
+                            frameData.localStorage = localStorage;
+                        }
+
+                        if (sessionStorage && options.sessionStorage) {
+                            frameData.sessionStorage = sessionStorage;
+                        }
+                    }
+
+                    if (frameData.localStorage || frameData.sessionStorage) {
+                        framesData[origin] = frameData;
+                    }
+                }
+
+                data.framesData = framesData;
+                break;
+            }
+        }
+
+        if (options && options.path) {
+            await fs.writeJson(options.path, data, { spaces: 2 });
+        }
+
+        return data;
+    });
+};
