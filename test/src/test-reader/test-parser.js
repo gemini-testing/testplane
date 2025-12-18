891194f45ed7bff4381be5b0737b8f692d2ae96f
rocketraccoon <rocketraccoon@yandex-team.ru>
2025-12-18 16:05:33 +0700

    fix: add test for bug case

--- test/src/test-reader/test-parser.js
+++ test/src/test-reader/test-parser.js
@@ -637,7 +637,7 @@ describe("test-reader/test-parser", () => {
                 assert.notCalled(TreeBuilder.prototype.addTestFilter);
             });
 
-            describe("if set", () => {
+            describe.only("if set", () => {
                 it("should set test filter to tree builder", async () => {
                     await parse_({ tag: compileTagFilter("smoke") });
 
@@ -684,6 +684,10 @@ describe("test-reader/test-parser", () => {
                             ["smoke", false],
                             ["slow", false],
                         ]),
+                        parent: {
+                            fullTitle: () => "Some parent",
+                            tags: new Map([]),
+                        },
                     };
 
                     assert.isFalse(filter(test));
