4b83889edb8fc65df1c799467c9b954d81f47fbd
shadowusr <58862284+shadowusr@users.noreply.github.com>
2026-04-16 23:36:46 +0300

    refactor: refactor client-bridge to support multiple instances and namespaces (#1235)
    * feat: implement type-safe isomorphic coords helpers
    
    * fix: fix rounding helper issues and add tests
    
    * ci: use github runners instead of self-hosted
    
    * ci: run standalone tests on github runners
    
    * ci: fix fixtures generation script
    
    * refactor: refactor client-bridge to support multiple instances and namespaces

--- src/browser/client-bridge/index.ts
+++ src/browser/client-bridge/index.ts
@@ -1,26 +1,98 @@
+import { inspect } from "node:util";
 import path from "path";
 import fs from "fs";
-import { ClientBridge } from "./client-bridge";
-import { ExistingBrowser } from "../existing-browser";
+import makeDebug from "debug";
+import { ClientBridgeError } from "./error";
+
+const debug = makeDebug("testplane:client-bridge");
 
 const bundlesCache: Record<string, string> = {};
 
-export { ClientBridge };
+interface Browser {
+    execute: <R>(command: string, ...args: unknown[]) => Promise<R>;
+}
 
-export const build = async (
-    browser: ExistingBrowser,
-    opts: { calibration?: { needsCompatLib?: boolean } } = {},
-): Promise<ClientBridge> => {
-    const needsCompatLib = opts.calibration?.needsCompatLib ?? false;
-    const scriptFileName = needsCompatLib ? "bundle.compat.js" : "bundle.native.js";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export class ClientBridge<T extends Record<string, (...args: any[]) => any>> {
+    private _browser: Browser;
+    private _script: string;
+    private _namespace: string;
 
-    if (bundlesCache[scriptFileName]) {
-        return ClientBridge.create(browser, bundlesCache[scriptFileName]);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    static async create<T extends Record<string, (...args: any[]) => any>>(
+        browser: Browser,
+        namespace: string,
+        opts: { needsCompatLib?: boolean },
+    ): Promise<ClientBridge<T>> {
+        const needsCompatLib = opts?.needsCompatLib ?? false;
+        const scriptFileName = needsCompatLib ? "bundle.compat.js" : "bundle.native.js";
+        const scriptFilePath = path.join(__dirname, "..", "client-scripts", namespace, "build", scriptFileName);
+
+        if (bundlesCache[scriptFilePath]) {
+            return new ClientBridge(browser, bundlesCache[scriptFilePath], namespace);
+        }
+
+        const bundle = await fs.promises.readFile(scriptFilePath, { encoding: "utf8" });
+        bundlesCache[scriptFilePath] = bundle;
+
+        return new this(browser, bundle, namespace);
     }
 
-    const scriptFilePath = path.join(__dirname, "..", "client-scripts", scriptFileName);
-    const bundle = await fs.promises.readFile(scriptFilePath, { encoding: "utf8" });
-    bundlesCache[scriptFileName] = bundle;
+    constructor(browser: Browser, script: string, namespace: string) {
+        this._browser = browser;
+        this._script = script;
+        this._namespace = namespace;
+    }
 
-    return ClientBridge.create(browser, bundle);
-};
+    async call<K extends keyof T>(name: K, args: Parameters<T[K]>): Promise<ReturnType<T[K]>> {
+        return this._callCommand<T>(this._clientMethodCommand(name, args), true) as ReturnType<T[K]>;
+    }
+
+    private async _callCommand<T>(command: string, shouldInjectScriptBeforeCall: boolean): Promise<T> {
+        try {
+            if (debug.enabled) {
+                debug(` > calling command ${command}`);
+            }
+
+            const result = await this._browser.execute<{ isClientScriptNotInjected?: boolean }>(command);
+
+            if (debug.enabled) {
+                debug(` < result for command ${command.slice(0, 256)}: ${inspect(result, { depth: null })}`);
+            }
+
+            if (!result || !result.isClientScriptNotInjected) {
+                return result as T;
+            }
+
+            if (shouldInjectScriptBeforeCall) {
+                await this._inject();
+                return this._callCommand<T>(command, false);
+            }
+
+            throw new ClientBridgeError("Unable to inject client script");
+        } catch (e) {
+            throw new ClientBridgeError(
+                `Failed to call command ${command} due to error: ${(e as Error)?.message ?? "Unknown error"}`,
+                { cause: e },
+            );
+        }
+    }
+
+    private _clientMethodCommand<K extends keyof T>(name: K, args: Parameters<T[K]>): string {
+        const params = args.map(arg => (arg !== undefined ? JSON.stringify(arg) : "undefined")).join(", ");
+        const call = `__geminiCore['${this._namespace}'].${String(name)}(${params})`;
+        return this._guardClientCall(call);
+    }
+
+    private _guardClientCall(call: string): string {
+        return `return (typeof __geminiCore !== "undefined" && typeof __geminiCore['${this._namespace}'] !== "undefined") ? ${call} : {isClientScriptNotInjected: true}`;
+    }
+
+    private async _inject(): Promise<void> {
+        debug(` > injecting script into namespace ${this._namespace}`);
+        if (debug.enabled) {
+            console.log(this._script);
+        }
+        await this._browser.execute(this._script, this._namespace);
+    }
+}
