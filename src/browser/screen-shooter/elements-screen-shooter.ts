8bba9d705c23f4680c75dd4406481e0cb7e3ac72
shadowusr <nikhakerlab@gmail.com>
2026-08-25 22:38:28 +0300

    chore: fix code formatting

--- src/browser/screen-shooter/elements-screen-shooter.ts
+++ src/browser/screen-shooter/elements-screen-shooter.ts
@@ -720,7 +720,11 @@ export class ElementsScreenShooter {
 
                 timeSpentOnCapture += performance.now() - captureStartTime;
 
-                if (isStrictAttempt && this._browserProperties.emulatedPixelRatio && opts.preferredPixelRatio !== undefined) {
+                if (
+                    isStrictAttempt &&
+                    this._browserProperties.emulatedPixelRatio &&
+                    opts.preferredPixelRatio !== undefined
+                ) {
                     const currentPixelRatio = await this._browserSideScreenshooter.call("getCurrentPixelRatio", []);
 
                     if (currentPixelRatio !== opts.preferredPixelRatio) {
